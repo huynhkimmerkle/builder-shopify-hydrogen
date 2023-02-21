382f0387fa583e0b22b0da5d7b0aa0e5c7ebf4b9
Huynh Kim <huynh.kim@isobar.com>
2023-02-21 11:15:55 +0700

    add collection card client

--- src/components/BuilderComponent.client.tsx
+++ src/components/BuilderComponent.client.tsx
@@ -1,8 +1,8 @@
-import {Builder, builder, BuilderComponent} from '@builder.io/react';
+import {builder, Builder, BuilderComponent} from '@builder.io/react';
 import "@builder.io/widgets";
 import { fetchSync } from '@shopify/hydrogen';
 import { Product } from '@shopify/hydrogen/storefront-api-types';
-import { Grid, ProductCard} from '~/components';
+import { Grid, ProductCard, CollectionCard} from '~/components';
 import { getImageLoadingPriority } from '~/lib/const';
 
 builder.init('3636687a3f434e1fb3bf09ca71639c49');
@@ -32,7 +32,7 @@ Builder.registerComponent(ProductByCollection, {
   ],
 });
 
-const BestSellers = ({ handle }) => {
+const BestSellers = () => {
   const products: Product[] = fetchSync('/api/bestSellers').json();
   console.log(products);
   return (
@@ -56,17 +56,35 @@ Builder.registerComponent(BestSellers, {
 
 const CollectionList = () => {
   const collections: Collection[] = fetchSync('/api/collectionListHandle').json();
-  console.log(collections, 'abc');
+  console.log(collections, 'log in component collection list');
   return (
-    <Grid items={collections.length === 3 ? 3 : 2}>
-      {collections.map((collection, i) => (
-        <div>{collection}</div>
-      ))}
-    </Grid>
+    <section>
+      <h2><strong>Collections</strong></h2>
+      <Grid items={collections.length === 3 ? 3 : 2}>
+        {collections.map((collection, i) => (
+          <CollectionCard
+            collection={collection}
+            key={collection.id}
+            loading={getImageLoadingPriority(i, 2)}
+          />
+        ))}
+      </Grid>
+    </section>
   );
 }
 Builder.registerComponent(CollectionList, {
   name: 'CollectionList'
+});
+
+const Hyperlink = function (props: any) {
+    return (
+        <h1>{props.name}</h1>
+    );
+}
+Builder.registerComponent(Hyperlink, {
+    name: 'Hyperlink',
+    inputs: [{ name: 'name', type: 'text' }],
+    image: 'https://tabler-icons.io/static/tabler-icons/icons-png/link.png'
 });
 
 export { ProductByCollection,BuilderComponent };
